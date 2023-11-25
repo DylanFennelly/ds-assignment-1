5ed507d841645d7ca4358b53414eb1d5089acc07
DylanFennelly <dylanantfennelly@gmail.com>
2023-11-25 11:02:01 +0000

    Restructured app to to multi-stack (auth and app stacks)

--- lib/ds-assignment-1-stack.ts
+++ lib/ds-assignment-1-stack.ts
@@ -1,28 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import * as custom from "aws-cdk-lib/custom-resources";
-import { generateBatch } from '../shared/util';
-import { reviews } from '../seed/reviews';
 import * as apig from "aws-cdk-lib/aws-apigateway";
 import { UserPool } from "aws-cdk-lib/aws-cognito";
-import * as node from "aws-cdk-lib/aws-lambda-nodejs";
+import { AuthApi } from './auth-api';
+import { AppApi } from './api-api';
 
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DsAssignment1Stack extends cdk.Stack {
-  private auth: apig.IResource;
-  private userPoolId: string;
-  private userPoolClientId: string;
-
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // ############
-    // ### AUTH ###
-    // ############
 
     //User pool
     const userPool = new UserPool(this, "Assign1UserPool", {
@@ -31,282 +17,22 @@ export class DsAssignment1Stack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    this.userPoolId = userPool.userPoolId;
+    const userPoolId = userPool.userPoolId;
 
     const appClient = userPool.addClient("Assign1AppClient", {
       authFlows: { userPassword: true },
     });
 
-    this.userPoolClientId = appClient.userPoolClientId;
+    const userPoolClientId = appClient.userPoolClientId;
 
-    const authApi = new apig.RestApi(this, "Assign1AuthServiceApi", {
-      description: "Authentication Service RestApi",
-      endpointTypes: [apig.EndpointType.REGIONAL],
-      defaultCorsPreflightOptions: {
-        allowOrigins: apig.Cors.ALL_ORIGINS,
-      },
+    new AuthApi(this, 'AuthServiceApi', {
+      userPoolId: userPoolId,
+      userPoolClientId: userPoolClientId,
     });
 
-    //Auth API
-    this.auth = authApi.root.addResource("auth");
-
-    //signup
-    this.addAuthRoute(
-      "signup",
-      "POST",
-      "SignupFn",
-      'signup.ts'
-    );
-
-    //confirm signup
-    this.addAuthRoute(
-      "confirm_signup",
-      "POST",
-      "ConfirmFn",
-      "confirm-signup.ts"
-    );
-
-    //signout
-    this.addAuthRoute('signout', 'GET', 'SignoutFn', 'signout.ts');
-
-    //signin
-    this.addAuthRoute('signin', 'POST', 'SigninFn', 'signin.ts');
-
-
-    // #####################
-    // ### DYNAMODB INIT ###
-    // #####################
-
-    //DynamoDB table
-    const reviewsTable = new dynamodb.Table(this, "reviewsTable", {
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      partitionKey: { name: "MovieId", type: dynamodb.AttributeType.NUMBER },
-      sortKey: { name: "ReviewDate", type: dynamodb.AttributeType.STRING },     // Adds a sort key to create a composite key
-      removalPolicy: cdk.RemovalPolicy.DESTROY,                                 // (allows a single MovieId to have multiple reviews [one per date])
-      tableName: "Reviews",                                                     // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_dynamodb.Table.html
+    new AppApi(this, 'AppApi', {
+      userPoolId: userPoolId,
+      userPoolClientId: userPoolClientId,
     });
-
-    //table seeding
-    new custom.AwsCustomResource(this, "reviewsddbInitData", {
-      onCreate: {
-        service: "DynamoDB",
-        action: "batchWriteItem",
-        parameters: {
-          RequestItems: {
-            [reviewsTable.tableName]: generateBatch(reviews)
-          },
-        },
-        physicalResourceId: custom.PhysicalResourceId.of("reviewsddbInitData"),
-      },
-      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
-        resources: [reviewsTable.tableArn]
-      }),
-    });
-
-    // #################
-    // ### FUNCTIONS ###
-    // #################
-
-    //get all movies for input movie ID
-    const getMovieReviewsFn = new lambdanode.NodejsFunction(
-      this,
-      "GetMovieReviewsFn",
-      {
-        architecture: lambda.Architecture.ARM_64,
-        runtime: lambda.Runtime.NODEJS_16_X,
-        entry: `${__dirname}/../lambda/getMovieReviews.ts`,
-        timeout: cdk.Duration.seconds(10),
-        memorySize: 128,
-        environment: {
-          TABLE_NAME: reviewsTable.tableName,
-          REGION: 'eu-west-1',
-        },
-      }
-    )
-
-    //add new movie review
-    const newReviewFn = new lambdanode.NodejsFunction(this, "AddReviewFn", {
-      architecture: lambda.Architecture.ARM_64,
-      runtime: lambda.Runtime.NODEJS_16_X,
-      entry: `${__dirname}/../lambda/addReview.ts`,
-      timeout: cdk.Duration.seconds(10),
-      memorySize: 128,
-      environment: {
-        TABLE_NAME: reviewsTable.tableName,
-        REGION: "eu-west-1",
-      },
-    });
-
-    //get movie reviews by reviewer name
-    const getMovieReviewsByAuthorFn = new lambdanode.NodejsFunction(this, "GetMovieReviewsByAuthorFn", {
-      architecture: lambda.Architecture.ARM_64,
-      runtime: lambda.Runtime.NODEJS_16_X,
-      entry: `${__dirname}/../lambda/getMovieReviewsByAuthor.ts`,
-      timeout: cdk.Duration.seconds(10),
-      memorySize: 128,
-      environment: {
-        TABLE_NAME: reviewsTable.tableName,
-        REGION: "eu-west-1",
-      },
-    });
-
-    //get all reviews by reviewer name
-    const getAllReviewsByAuthorFn = new lambdanode.NodejsFunction(this, "GetAllReviewsByAuthorFn", {
-      architecture: lambda.Architecture.ARM_64,
-      runtime: lambda.Runtime.NODEJS_16_X,
-      entry: `${__dirname}/../lambda/getAllReviewsByAuthor.ts`,
-      timeout: cdk.Duration.seconds(10),
-      memorySize: 128,
-      environment: {
-        TABLE_NAME: reviewsTable.tableName,
-        REGION: "eu-west-1",
-      },
-    });
-
-    //update review
-    const updateReviewFn = new lambdanode.NodejsFunction(this, "UpdateReviewFn", {
-      architecture: lambda.Architecture.ARM_64,
-      runtime: lambda.Runtime.NODEJS_16_X,
-      entry: `${__dirname}/../lambda/updateMovieReview.ts`,
-      timeout: cdk.Duration.seconds(10),
-      memorySize: 128,
-      environment: {
-        TABLE_NAME: reviewsTable.tableName,
-        REGION: "eu-west-1",
-      },
-    });
-
-
-    //authorizer
-    const authorizerFn = new node.NodejsFunction(this, "AuthorizerFn", {
-      architecture: lambda.Architecture.ARM_64,
-      runtime: lambda.Runtime.NODEJS_16_X,
-      entry: `${__dirname}/../lambda/auth/authorizer.ts`,
-      timeout: cdk.Duration.seconds(10),
-      memorySize: 128,
-      environment: {
-        USER_POOL_ID: this.userPoolId,
-        CLIENT_ID: this.userPoolClientId,
-        TABLE_NAME: reviewsTable.tableName,
-        REGION: "eu-west-1",
-      },
-    });
-
-    //request authorizer
-    const requestAuthorizer = new apig.RequestAuthorizer(
-      this,
-      "RequestAuthorizer",
-      {
-        identitySources: [apig.IdentitySource.header("cookie")],
-        handler: authorizerFn,
-        resultsCacheTtl: cdk.Duration.minutes(0),
-      }
-    );
-
-    //table permissions
-    reviewsTable.grantReadData(getMovieReviewsFn)
-    reviewsTable.grantReadWriteData(newReviewFn)
-    reviewsTable.grantReadData(getMovieReviewsByAuthorFn)
-    reviewsTable.grantReadData(getAllReviewsByAuthorFn)
-    reviewsTable.grantReadWriteData(updateReviewFn)
-
-    // #####################
-    // ### API ENDPOINTS ###
-    // #####################
-
-    //REST API
-    const api = new apig.RestApi(this, "RestAPI", {
-      description: "Assignment 1 API",
-      deployOptions: {
-        stageName: "dev",
-      },
-      //CORS
-      defaultCorsPreflightOptions: {
-        allowHeaders: ["Content-Type", "X-Amz-Date"],
-        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
-        allowCredentials: true,
-        allowOrigins: ["*"],
-      },
-    }
-    )
-
-    //API root - all endpoints branch from this
-    const moviesEndpoint = api.root.addResource("movies");
-
-    //movie reviews (for post)
-    const reviewsEndpoint = moviesEndpoint.addResource("reviews")
-    reviewsEndpoint.addMethod(
-      "POST",
-      new apig.LambdaIntegration(newReviewFn, { proxy: true }),
-      {
-        authorizer: requestAuthorizer,
-        authorizationType: apig.AuthorizationType.CUSTOM,
-      }
-    )
-
-    //all reviews by reviewer
-    const getAllReviewsByAuthorEndpoint = reviewsEndpoint.addResource("{reviewerName}")
-    getAllReviewsByAuthorEndpoint.addMethod(
-      "GET",
-      new apig.LambdaIntegration(getAllReviewsByAuthorFn, { proxy: true })
-    )
-
-
-    //specific movie
-    const movieIdEndpoint = moviesEndpoint.addResource("{movieId}");
-
-    //speicifc movie reviews
-    const movieReviewsEndpoint = movieIdEndpoint.addResource("reviews");
-    movieReviewsEndpoint.addMethod(
-      "GET",
-      new apig.LambdaIntegration(getMovieReviewsFn, { proxy: true })
-    )
-
-    //specific move reviews by reviewer or year
-    const movieReviewsByAuthorEndpoint = movieReviewsEndpoint.addResource("{reviewerName}");
-    movieReviewsByAuthorEndpoint.addMethod(
-      "GET",
-      new apig.LambdaIntegration(getMovieReviewsByAuthorFn, { proxy: true })
-    )
-
-    movieReviewsByAuthorEndpoint.addMethod(
-      "PUT",
-      new apig.LambdaIntegration(updateReviewFn, { proxy: true }),
-      {
-        authorizer: requestAuthorizer,
-        authorizationType: apig.AuthorizationType.CUSTOM,
-      }
-    )
-    
   }
-
-  private addAuthRoute(   //private method to reduce code duplicate
-    resourceName: string,
-    method: string,
-    fnName: string,
-    fnEntry: string,
-    allowCognitoAccess?: boolean
-  ): void {
-    const commonFnProps = {
-      architecture: lambda.Architecture.ARM_64,
-      timeout: cdk.Duration.seconds(10),
-      memorySize: 128,
-      runtime: lambda.Runtime.NODEJS_16_X,
-      handler: "handler",
-      environment: {
-        USER_POOL_ID: this.userPoolId,
-        CLIENT_ID: this.userPoolClientId,
-        REGION: cdk.Aws.REGION
-      },
-    };
-    
-    const resource = this.auth.addResource(resourceName);
-    
-    const fn = new node.NodejsFunction(this, fnName, {
-      ...commonFnProps,
-      entry: `${__dirname}/../lambda/auth/${fnEntry}`,
-    });
-
-    resource.addMethod(method, new apig.LambdaIntegration(fn));
-  }  // end private method
 }
